cdc6516d9edf2e818f86e548ca257181bce56ab0
fengling <fengling@maihaoche.com>
2018-09-10 13:35:27 +0800

    多行文本裁剪

--- src/App.vue
+++ src/App.vue
@@ -45,6 +45,9 @@
       <section data-title="模块标题">
         <BlockHeader :blockTitle="'申请信息'" :size="2" />
       </section>
+      <section data-title="多行文本裁剪">
+        <ClampLines :width="'400px'" :lines="3" />
+      </section>
     </div>
   </div>
 </template>
@@ -62,6 +65,7 @@
   import MyWorkbench from './pc/myWorkbench/MyWorkbench.vue'
   import Echarts from './pc/Echarts.vue'
   import BlockHeader from './pc/BlockHeader.vue'
+  import ClampLines from './pc/ClampLines.vue'
 
   const jsonData = '[{"items":[{"market_type":"forexdata","symbol":"XAUUSD"},{"market_type":"forexdata","symbol":"UKOIL"},{"market_type":"forexdata","symbol":"CORN"}],"name":""},{"items":[{"market_type":"forexdata","symbol":"XAUUSD"},{"market_type":"forexdata","symbol":"XAGUSD"},{"market_type":"forexdata","symbol":"AUTD"},{"market_type":"forexdata","symbol":"AGTD"}],"name":"贵金属"},{"items":[{"market_type":"forexdata","symbol":"CORN"},{"market_type":"forexdata","symbol":"WHEAT"},{"market_type":"forexdata","symbol":"SOYBEAN"},{"market_type":"forexdata","symbol":"SUGAR"}],"name":"农产品"},{"items":[{"market_type":"forexdata","symbol":"UKOIL"},{"market_type":"forexdata","symbol":"USOIL"},{"market_type":"forexdata","symbol":"NGAS"}],"name":"能源化工"}]'
   export default {
@@ -162,7 +166,8 @@
       Lodding,
       CountDown,
       Echarts,
-      BlockHeader
+      BlockHeader,
+      ClampLines
     },
     methods: {
       getChange (pri, city, area) {
---
+++ src/pc/ClampLines.vue
@@ -0,0 +1,39 @@
+<template>
+<Poptip trigger="hover" :content="textContent" word-wrap width="400">
+  <div class="ClampLines" :style="{width: this.width,'-webkit-line-clamp':this.lines}">
+    {{textContent}}
+  </div>
+</Poptip>
+</template>
+
+<script>
+export default {
+  name: 'ClampLines',
+  props: {
+    textContent: {
+      type: String,
+      default: 'Zent ( \ˈzent\ ) 是有赞 PC 端 WebUI 规范的 React 实现，提供了一整套基础的 UI 组件以及一些常用的业务组件。目前我们有 50+ 组件，这些组件都已经在有赞的各类 PC 业务中使用，我们会在此基础上，持续开发一些新组件。我们的目标是让 React 项目开发更快、更简单。'
+    },
+    width: {
+      type: Number
+    },
+    lines: {
+      type: Number,
+      default: 2
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.ClampLines {
+  text-overflow: -o-ellipsis-lastline;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  :after {
+    content: '...'
+  }
+}
+</style>
